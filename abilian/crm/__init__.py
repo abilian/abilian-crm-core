a755518ab569bcfeb14b912c7708444b054e8e9e
Bertrand Mathieu <bmathieu@abilian.com>
2015-03-24 17:37:48 +0100

    initial installable package

---
+++ abilian/crm/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+"""
+Abilian CRM package
+"""
+from __future__ import absolute_import
---
+++ setup.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+"""
+"""
+from __future__ import absolute_import
+
+import setuptools
+from distutils.command.build import build as _build
+from setuptools.command.sdist import sdist as _sdist
+from setuptools.command.develop import develop as _develop
+
+import pip
+
+session = session=pip.download.PipSession()
+_install_requires = pip.req.parse_requirements(
+    'requirements.txt', session=session)
+install_requires = [str(ir.req) for ir in _install_requires]
+
+
+class build(_build):
+  sub_commands = [('compile_catalog', None)] + _build.sub_commands
+
+
+class sdist(_sdist):
+  sub_commands = [('compile_catalog', None)] + _sdist.sub_commands
+
+
+class develop(_develop):
+  def run(self):
+    _develop.run(self)
+    self.run_command('compile_catalog')
+
+
+setuptools.setup(
+  name='abilian-crm',
+  use_scm_version=True,
+  url='http://docs.abilian.com/',
+  license='LGPL',
+  author='Abilian SAS',
+  author_email='contact@abilian.com',
+  description='Social Business / Enterprise Social Networking platform',
+  packages=['abilian.crm'],
+  zip_safe=False,
+  platforms='any',
+  setup_requires=['babel', 'setuptools-git', 'setuptools_scm'],
+  install_requires=install_requires,
+  extras_require={},
+  include_package_data=True,
+  classifiers=[
+    'Private :: Do Not Upload',     # not published for now
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Framework :: Flask',
+  ],
+  cmdclass={
+    'build': build,
+    'sdist': sdist,
+    'develop': develop,
+  },
+)
