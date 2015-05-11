150d9319b5f41f95482322058806fcfef9816fed
Bertrand Mathieu <bmathieu@abilian.com>
2015-05-11 15:06:14 +0200

    postal address: allow 'multiple'

--- abilian/crm/generator/fieldtypes/postaladdress.py
+++ abilian/crm/generator/fieldtypes/postaladdress.py
@@ -4,8 +4,11 @@
 from __future__ import absolute_import
 
 import sqlalchemy as sa
+import abilian.web.forms.fields as awbff
+import abilian.web.forms.widgets as aw_widgets
+
 from ...models import PostalAddress
-from ...forms import PostalAddressField
+from ...forms import PostalAddressField, PostalAddressForm
 from .registry import model_field, form_field
 from .base import Field, FormField
 
@@ -15,12 +18,16 @@ class _PostalAddressField(Field):
   __fieldname__ = 'PostalAddress'
   sa_type = sa.Integer
   default_ff_type = 'PostalAddressFormField'
-  allow_multiple = False
+  allow_multiple = True
 
   def get_model_attributes(self, *args, **kwargs):
     # column declared_attr
     col_name = self.name + '_id'
 
+    if self.multiple:
+      yield self.gen_m2m(*args, **kwargs)
+      raise StopIteration
+    
     def gen_column(cls):
       fk_kw = dict(
         name=u'{}_{}_fkey'.format(cls.__name__.lower(), col_name),
@@ -44,9 +51,47 @@ class _PostalAddressField(Field):
 
     gen_relationship.func_name = self.name
     yield self.name, sa.ext.declarative.declared_attr(gen_relationship)
-        
 
+  def gen_m2m(self, *args, **kwargs):
+    model_name = self.model
+
+    def gen_relationship(cls):
+      src_name = cls.__tablename__
+      local_src_col = model_name.lower() + '_id'
+      local_target_col = 'postaladdress_id'
+      tbl_name = src_name + '_' + self.name
+      secondary_table = sa.Table(
+        tbl_name,
+        cls.metadata,
+        sa.Column(local_src_col, sa.ForeignKey(cls.id)),
+        sa.Column(local_target_col, sa.ForeignKey(PostalAddress.id)),
+        sa.schema.UniqueConstraint(local_src_col, local_target_col),        
+      )
+
+      rel_kw = dict(secondary=secondary_table)
+      return sa.orm.relationship(PostalAddress, **rel_kw)
+
+    gen_relationship.func_name = self.name
+    return self.name, sa.ext.declarative.declared_attr(gen_relationship)
+    
+    
 @form_field
 class PostalAddressFormField(FormField):
   ff_type = PostalAddressField
+  
+  def get_type(self, *args, **kwargs):
+    return (awbff.ModelFieldList if self.multiple else self.ff_type)
+
+  def get_extra_args(self, *args, **kwargs):
+    extra_args = super(PostalAddressFormField, self)\
+                    .get_extra_args(*args, **kwargs)
+    if self.multiple:
+      extra_args['unbound_field'] = awbff.ModelFormField(PostalAddressForm)
+      extra_args['min_entries'] = 1
+      extra_args['population_strategy'] = 'update'
+      extra_args['widget'] = aw_widgets.TabularFieldListWidget(
+        template='widgets/model_fieldlist.html',
+      )
+
+    return extra_args
   
