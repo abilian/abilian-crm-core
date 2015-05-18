7b7fe2430fb82c656dcb447e0a4d6b1d7cbb0fe5
Bertrand Mathieu <bmathieu@abilian.com>
2015-05-18 18:09:10 +0200

    phonenumberwidget: add 'phone' icon

--- abilian/crm/widgets.py
+++ abilian/crm/widgets.py
@@ -3,16 +3,18 @@
 """
 from __future__ import absolute_import
 
-from wtforms.widgets.core import TextInput
+from abilian.web.forms.widgets import TextInput
+from abilian.web.action import FAIcon
 
 from .jinja_filters import format_phonenumber
 
 
 class PhoneNumberWidget(TextInput):
+  pre_icon = FAIcon('phone')
 
   def render_view(self, field, **kwargs):
     data = field.data
     if not data:
       return u''
-    
+
     return format_phonenumber(data, international=True)
