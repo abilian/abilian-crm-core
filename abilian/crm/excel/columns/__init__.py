e586afc6420f0b9baee59353ae70d90bb399e07c
Bertrand Mathieu <bmathieu@abilian.com>
2015-10-26 11:47:03 +0100

    add TagsColumn

--- abilian/crm/excel/columns/__init__.py
+++ abilian/crm/excel/columns/__init__.py
@@ -7,6 +7,17 @@ from .base import Column, ColumnSet, Invalid
 
 from .dates import DateTimeColumn, DateColumn
 from .vocabulary import VocabularyColumn
+from .tags import TagsColumn
 from .postaladdress import PostalAddressColumn
 from .misc import TextIntegerColumn
 from .related import RelatedColumnSet, ManyRelatedColumnSet
+
+__all__ = [
+  'Column', 'ColumnSet', 'Invalid',
+  'DateTimeColumn', 'DateColumn',
+  'VocabularyColumn',
+  'TagsColumn',
+  'PostalAddressColumn',
+  'TextIntegerColumn',
+  'RelatedColumnSet', 'ManyRelatedColumnSet',
+]
