6ad9cff2e9ca9c04bcd82fb1d09b84fd552c76ac
Stefane Fermigier <sf@fermigier.com>
2017-08-02 13:51:27 +0200

    Unicode literals (1).

--- abilian/crm/excel/tasks.py
+++ abilian/crm/excel/tasks.py
@@ -2,7 +2,7 @@
 """
 Celery task for async export
 """
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, print_function, unicode_literals
 
 from cStringIO import StringIO
 from itertools import ifilter
